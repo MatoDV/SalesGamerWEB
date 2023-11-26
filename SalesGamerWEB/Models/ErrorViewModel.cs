552d29d10fb4ade0b8c3d12e31db69a3f72f9a57
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-11-26 12:57:29 -0300

    feat: creation of the web

---
+++ SalesGamerWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesGamerWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
